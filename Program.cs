e0faa73d88fdb6bd34e6d79ecd972932a53b4552
EddyBautista <bautista.j.eddy@gmail.com>
2021-01-11 07:08:22 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeQuestions_Jan2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
